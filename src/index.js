1d1dd43252264944585e2ee7f015449ec689e248
CarLoWorld <ben.carlo.cornico@gmail.com>
2022-04-13 23:01:14 -0700

    adding project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
+
 let projectList = [];
-class Project {
-    constructor(name, tasks) {
-        this.name = name;
-        this.availableTasks = [];
-        this.fnishedTasks = [];
+
+
+//these are buttons to show the modal forms
+const btnModalAddProject = document.querySelector('.btn-add-project');
+const btnModalAddTask = document.querySelector('.btn-add-task');
+
+const addProjectModalForm = document.querySelector('.project-form')
+const modalContainer = document.querySelector('.modal-container');
+const modalAddTask = document.querySelector('.modal-add-task');
+const modalAddProject = document.querySelector('.modal-add-project');
+
+const btnCancel = document.querySelectorAll('.btn-cancel');
+const btnAddProject = document.querySelector('.modal-btn-add-project');
+
+
+btnModalAddTask.addEventListener('click', () => {
+    modalContainer.style.display = "flex";
+    modalAddTask.style.display = "grid";
+});
+
+btnModalAddProject.addEventListener('click', () => {
+    modalContainer.style.display = "flex";
+    modalAddProject.style.display = "grid";
+});
+
+//CLOSING THE MODAL
+window.addEventListener('click', (e) => {
+    if(e.target == modalContainer) {
+        closeModal()
     }
+})
+btnCancel.forEach(btn => btn.addEventListener('click', closeModal));
+function closeModal() {
+    modalContainer.style.display = "none";
+    modalAddTask.style.display = "none";
+    modalAddProject.style.display = "none";;
 }
 
+// LOAD ALL PROJECTS
+function loadProject() {
+    const projectListContainer = document.querySelector('.project-list');
+    projectListContainer.innerHTML = "";
+    projectList.forEach((project, index) => {
+        const card = createProjectCard(project, index);
+        projectListContainer.appendChild(card);
+    })
+}
+
+function createProjectCard(title, key) {
+    const projectCard = document.createElement('div');
+    projectCard.setAttribute('key', key);
+    projectCard.classList.add('card-project');
+    const projectTitle = document.createElement('h2');
+    projectTitle.innerText = title;
+    projectCard.append(projectTitle);
+    return projectCard;
+}
+
+// TODO LIST FUNCTIONS
 class Tasks {
     constructor(name, priority) {
         this.name = name;
@@ -14,45 +66,58 @@ class Tasks {
         this.isDone = isDone;
     }
 }
-
-function addProject(name) {
-
-}
-const btnAddProject = document.querySelector('.btn-add-project');
-const btnAddTask = document.querySelector('.btn-add-task');
-
-btnAddTask.addEventListener('click', () => {
-    const modalContainer = document.querySelector('.modal-container');
-    const modalAddTask = document.querySelector('.modal-add-task');
-
-    modalContainer.classList.add('active');
-    modalAddTask.classList.add('modal-active');
-});
-
-btnAddProject.addEventListener('click', () => {
-    const modalContainer = document.querySelector('.modal-container');
-    const modalAddProject = document.querySelector('.modal-add-project');
-
-    modalContainer.classList.add('active');
-    modalAddProject.classList.add('modal-active');
-});
-
-function makeAddProjectForm(newProject) {
-    //adding proiject
+class Project {
+    constructor(title) {
+        this.title = title;
+        this.availableTasks = [];
+        this.finishedTasks = [];
+    }
+    addTaskToProject(newTask) {
+        //addtask
+    }
+    updateTask(task) {
+        //updatetask
+    }
+    deleteTask(task) {
+        //deleete task
+    }
 }
 
-function addTaskToProject(project, task) {
-    //adding task to project;
+btnAddProject.addEventListener('click', addProjectObject);
+addProjectModalForm.addEventListener('submit', addProjectObject);
+
+function addProjectObject(event) {
+    event.preventDefault();
+    const projectTitle = document.querySelector('#projectTitle').value;
+    if(projectTitle !== null || projectTitle !== "") {
+        const newProject = new Project(projectTitle);
+        projectList.push(projectTitle);
+        closeModal();
+        loadProject();
+        console.log(newProject)
+    };
 }
 
-function updateTask(project, task) {
-    //done task
-}
+// function addProject(projectTitle) {
+//     projectList.push(projectTitle)
+// }
 
-function deleteProject(project) {
-    //delete project
-}
+// function makeAddProjectForm(newProject) {
+//     //adding proiject
+// }
 
-function deleteTask(project, task) {
-    //delete task
-} 
+// function addTaskToProject(project, task) {
+//     //adding task to project;
+// }
+
+// function updateTask(project, task) {
+//     //done task
+// }
+
+// function deleteProject(project) {
+//     //delete project
+// }
+
+// function deleteTask(project, task) {
+//     //delete task
+// } 
