0402b8364554b2acf7325b05a77cd0d503d63593
CarLoWorld <ben.carlo.cornico@gmail.com>
2022-04-14 22:39:06 -0700

    loading avaiable tasks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,75 @@
 
-let projectList = [];
+let projectList = [
+    {
+        title: "HELLO",
+        availableTasks: [
+            {
+                name: "TASK 1",
+                priority:  "medium"
+            },
+            {
+                name: "TASK 2",
+                priority: "high"
+            }
+        ],
+        finishedTasks: [
+            {
+                name: "FINISHED TASK 3",
+                priority: "low"
+            },
+            {
+                name: "FINISHED TASK 4",
+                priority: "medium"
+            } 
+        ]
+    },
+    {
+        title: "WORLD",
+        availableTasks: [
+            {
+                name: "WROLD TASK 1",
+                priority:  "low"
+            },
+            {
+                name: "WORLD TASK 2",
+                priority: "medium"
+            }
+        ],
+        finishedTasks: [
+            {
+                name: "WORLD FINISHED TASK 3",
+                priority: "low"
+            },
+            {
+                name: "WORLD FINISHED TASK 4",
+                priority: "HIGH"
+            } 
+        ]
+    },
+    {
+        title: "TESTING",
+        availableTasks: [
+            {
+                name: "TESTING TASK 1",
+                priority:  "high"
+            },
+            {
+                name: "TESTING TASK 2",
+                priority: "low"
+            }
+        ],
+        finishedTasks: [
+            {
+                name: "TESTING FINISHED TASK 3",
+                priority: "medium"
+            },
+            {
+                name: "TESTING FINISHED TASK 4",
+                priority: "medium"
+            } 
+        ]
+    }
+];
 
 
 //these are buttons to show the modal forms
@@ -38,25 +108,91 @@ function closeModal() {
     modalAddProject.style.display = "none";;
 }
 
+
+// WHEN SELECTINGA PROJECT CARD
+function projectCardSelect(event) {
+    const projectCards = document.querySelectorAll('.card-project');
+    projectCards.forEach(card => card.classList.remove('project-active'))
+    const activeProject = event.target;
+    activeProject.classList.add('project-active');
+
+    loadTask(activeProject.getAttribute("key"));
+    
+}
+function createProjectCard(title, key) {
+    const projectCard = document.createElement('div');
+    projectCard.classList.add('card-project');
+    projectCard.setAttribute('key', key);
+    projectCard.innerText = title;
+    return projectCard;
+}
+
 // LOAD ALL PROJECTS
 function loadProject() {
     const projectListContainer = document.querySelector('.project-list');
     projectListContainer.innerHTML = "";
     projectList.forEach((project, index) => {
-        const card = createProjectCard(project, index);
+        const card = createProjectCard(project.title, index);
         projectListContainer.appendChild(card);
+        card.addEventListener('click', projectCardSelect)
     })
 }
 
-function createProjectCard(title, key) {
-    const projectCard = document.createElement('div');
-    projectCard.setAttribute('key', key);
-    projectCard.classList.add('card-project');
-    const projectTitle = document.createElement('h2');
-    projectTitle.innerText = title;
-    projectCard.append(projectTitle);
-    return projectCard;
+loadProject()
+
+function loadTask(key) {
+    const taskListContainer = document.querySelector('.taskslist-container');
+    taskListContainer.innerHTML = ""
+    const projectTitle = projectList[key].title;
+    const availableTasks = projectList[key].availableTasks;
+    const finishedTasks = projectList[key].finishedTasks;
+    availableTasks.forEach(aTask => {
+        const task = createAvailableTaskCards(aTask.name, aTask.priority);
+        console.log(task)
+        taskListContainer.append(task);
+    })
+
 }
+
+function createAvailableTaskCards(name, priority) {
+    const taskCard = document.createElement('div');
+
+    const btnDone = document.createElement('button');
+    btnDone.classList.add('btn-done');
+    const btnDelete = document.createElement('button');
+    btnDelete.classList.add('btn-delete');
+
+    const btnDeleteText = document.createElement('span');
+    btnDeleteText.classList.add('mdi');
+    btnDeleteText.classList.add('mdi-close');
+    btnDelete.append(btnDeleteText);
+    btnDone.innerHTML = "Done";
+
+    const btnGroup = document.createElement('div');
+    btnGroup.classList.add('btn-group');
+    btnGroup.appendChild(btnDone);
+    btnGroup.appendChild(btnDelete);
+
+    taskCard.classList.add('card-task');
+    taskCard.classList.add(priority);
+    const taskName = document.createElement('p');
+    taskName.innerText = name;
+    taskCard.appendChild(taskName);
+    taskCard.appendChild(btnGroup)
+
+    return taskCard;
+}
+
+{/* <div class="card-task urgent">
+                        <p class="task-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, dolore!</p>
+                        <div class="btn-group">
+                            <button class="btn-done">Done</button>
+                            <button class="btn-delete">
+                                <span class="mdi mdi-close"></span>
+                            </button>
+                        </div>
+                    </div> */}
+
 
 // TODO LIST FUNCTIONS
 class Tasks {
@@ -90,11 +226,9 @@ function addProjectObject(event) {
     event.preventDefault();
     const projectTitle = document.querySelector('#projectTitle').value;
     if(projectTitle !== null || projectTitle !== "") {
-        const newProject = new Project(projectTitle);
-        projectList.push(projectTitle);
+        projectList.push(new Project(projectTitle));
         closeModal();
         loadProject();
-        console.log(newProject)
     };
 }
 
